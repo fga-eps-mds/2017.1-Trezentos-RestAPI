8ccbfa79d4593318d48f339eecca7d27768d8514
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-03-30 17:03:54 -0300

    [DB] Changing database URL

--- config/db-config.js
+++ config/db-config.js
@@ -1,10 +1,24 @@
 module.exports = (env) => {
   var mongoose = require('mongoose')
-  mongoose.connect('mongodb://heroku_dh63xpgs:heroku_dh63xpgs@ds145790.mlab.com:45790/heroku_dh63xpgs')
+
+  var databaseUri = 'mongodb://heroku_z9l4c0x1:gan4pt3hnlr6kblgjg7l3qn7o8@ds135700.mlab.com:35700/heroku_z9l4c0x1'
+  
+  // env.MONGODB_URI || 'mongodb://'
+  //   + env.database.host + ':'
+  //   + env.database.port + '/'
+  //   + env.database.name
+
+  mongoose.connect(databaseUri, function (err, res) {
+    if (err) {
+      console.log('ERROR carai connecting to: ' + databaseUri + '. ' + err);
+    } else {
+      console.log('top Succeeded connected to: ' + databaseUri);
+    }
+  })
 
   var db = mongoose.connection
 
   db.on('open', () => {
-    console.log('Conected in mongodb://heroku_dh63xpgs:heroku_dh63xpgs@ds145790.mlab.com:45790/heroku_dh63xpgs')
+    console.log('Conected in ' + databaseUri)
   })
 }
