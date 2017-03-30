975bbf7cc4956f987342888abace5bac78bcfd1c
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-03-30 11:30:41 -0300

    [DB] fixing database connection

--- config/db-config.js
+++ config/db-config.js
@@ -1,16 +1,10 @@
 module.exports = (env) => {
   var mongoose = require('mongoose')
-  mongoose.connect('mongodb://'
-    + env.database.host + ':'
-    + env.database.port + '/'
-    + env.database.name)
+  mongoose.connect('mongodb://heroku_dh63xpgs:heroku_dh63xpgs@ds145790.mlab.com:45790/heroku_dh63xpgs')
 
   var db = mongoose.connection
 
   db.on('open', () => {
-    console.log('Conected in mongodb://'
-      + env.database.host + ':'
-      + env.database.port + '/'
-      + env.database.name)
+    console.log('Conected in mongodb://heroku_dh63xpgs:heroku_dh63xpgs@ds145790.mlab.com:45790/heroku_dh63xpgs')
   })
 }
