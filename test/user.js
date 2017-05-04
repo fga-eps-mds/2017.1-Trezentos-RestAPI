0cb38baae6502548cd72e5053c54fd454cf98584
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-05-03 21:25:57 -0300

    [Test] Creates user test case

---
+++ test/user.js
@@ -0,0 +1,46 @@
+var expect = require("chai").expect
+var User = require('../model/schema/user')
+var userController = require('../controller/user.js')
+var sinon = require('sinon')
+
+const response = {
+  status: function () {},
+  end: function () {},
+  send: function () {}
+}
+
+describe('Test suite for Users', () => {
+  describe('User', () => {
+    describe('when registered', () => {
+
+      it('should successfully register user', (done) => {
+        var mock = sinon.mock(response)
+        mock.expects('status').once().withExactArgs(200).returns(response)
+        mock.expects('send').once().withExactArgs({ code: '200', message: 'user sucessfully registered' })
+
+        var query = {
+          name: 'test',
+          email: 'test@test.com',
+          password: '123456',
+          isFromFacebook: false,
+          salt: '123'
+        }
+
+        userController.register({ query: query }, response)
+        .then(() => {
+          try {
+            mock.verify()
+            done()
+          } catch (exception) {
+            done(exception)
+          }
+        })
+        .catch((err) => {
+          done(err)
+        })
+
+      })
+    })
+  })
+})
+
