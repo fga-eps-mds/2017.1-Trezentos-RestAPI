8583a139eec677e6d80faa6e5c12bffec5c8f4b9
Cecilia Dib <ceciliabessa@hotmail.com>
2017-05-21 14:28:36 -0300

    [TEST] Exam Controller Test
    Signed-off-by: Cecilia Dib <ceciliabessa@hotmail.com>
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>

--- test/exam.js
+++ test/exam.js
@@ -11,11 +11,11 @@ const response = {
 }
 
 describe('Test suite for Exams', () => {
+
   testHelper.setupTest()
 
   describe('Exam', () => {
       describe('when registered', () => {
-
         it('should successfully register exam', (done) => {
           var query = {
             name: 'testExam',
@@ -46,6 +46,39 @@ describe('Test suite for Exams', () => {
             done(err)
           })
         })
+
+        it('should successfully if exam already exists', (done) => {
+          var query = {
+            name: 'testExam',
+            userClassName: 'test',
+            classOwnerEmail: 'test@test.com'
+          }
+
+          var savedExam = {
+            name: 'testExam',
+            userClassName: 'test',
+            classOwnerEmail: 'test@test.com'
+          }
+
+          var mock = sinon.mock(response)
+          mock.expects('status').once().withExactArgs(200).returns(response)
+          mock.expects('send').once().withArgs({ code: 11000, message: 'exam already exists'})
+
+          examController.register({ query: query }, response)
+          .then(() => {
+            try {
+              mock.verify()
+              done()
+            } catch (exception) {
+              done(exception)
+            }
+          })
+          .catch((err) => {
+            done(err)
+          })
+
+        })
+
       })
   })
 })
